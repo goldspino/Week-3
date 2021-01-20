f510c7315fac377a0ff65a98dc63c63783a24ceb
Sathish Ramachandran <sathish.r26@gmail.com>
2021-01-20 02:06:14 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Challenge 1
+x = 100
+
+while (x >= 0):
+  print(x)
+  x -= 5
+  if (x == 50):
+    break
+print("I am out of the loop.")
+
+#Challenge 2
+x = 10
+
+while (x < 100):
+  x = x + 10
+
+print (x)
+
+#Challenge 3
+x = 10
+
+while (x < 100):
+  x = x + 10
+
+print (x)
